f30de0277704a57c16b8c1db7a6856eb38152f92
olety nikhilesh <oletynikhilesh.oletynagendra@syncfusion.com>
2020-03-18 18:34:35 +0530

    WPF-51171 How to load ListBox as drop-down content of WPF DropDownButtonAdv?

---
+++ DropDownButtonAdv/DropDownButtonAdv/ViewModel/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropDownButtonAdv
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            items = new ObservableCollection<string>();
+            items.Add("Item1");
+            items.Add("Item2");
+            items.Add("Item3");
+            items.Add("Item4");
+            items.Add("Item5");
+            items.Add("Item6");
+            items.Add("Item7");
+            items.Add("Item8");
+        }
+
+        private ObservableCollection<String> items;
+
+        public ObservableCollection<String> Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        private string selectedItem = "Item1";
+
+        public string SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; OnPropertyChanged("SelectedItem"); OnSelectedChanged(value); }
+        }
+
+        private void OnSelectedChanged(string SelectedItem)
+        {
+            //will invoke when Selection changes
+        }
+
+    }
+}
